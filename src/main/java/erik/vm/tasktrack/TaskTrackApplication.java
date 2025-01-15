fd7d962c6c5d4e4dbeb1cad0d3394489b012ef63
Erik Vainumäe <erik.vainumae@gmail.com>
2025-01-15 20:49:18 +0200

    initial commit

---
+++ src/main/java/erik/vm/tasktrack/TaskTrackApplication.java
@@ -0,0 +1,13 @@
+package erik.vm.tasktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erik/vm/tasktrack/TaskTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package erik.vm.tasktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
